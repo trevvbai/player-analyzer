4f27a97b06c5d50f7e502cc0fb1ae2cc7d4e65dc
tbailey <t.bailey@embarkwithus.com>
2024-07-28 22:25:13 -0500

    get tutorial app in working state

--- player-analyzer/src/app/app.component.ts
+++ player-analyzer/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'player-analyzer';
+  title = 'default';
 }
