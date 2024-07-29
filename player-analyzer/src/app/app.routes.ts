2352ca288f67b462bf6523fd5243918d04dec9c2
tbailey <t.bailey@embarkwithus.com>
2024-07-28 23:36:12 -0500

    completed tutorial with image buge

--- player-analyzer/src/app/app.routes.ts
+++ player-analyzer/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+
+const routes: Routes = [
+    {
+      path: '',
+      component: HomeComponent,
+      title: 'Home page',
+    },
+    {
+      path: 'details/:id',
+      component: DetailsComponent,
+      title: 'Home details',
+    },
+  ];
+
+  export default routes;
