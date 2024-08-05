a289707c85816a4f5dea420b8d890793f0b7d0ae
tbailey <t.bailey@embarkwithus.com>
2024-08-05 00:28:32 -0500

    feat: do some crazy scrollbar stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, Renderer2, SimpleChange} from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import {SupabaseService} from "./supabase.service";
@@ -7,6 +7,8 @@ import {AuthComponent} from "./auth/auth.component";
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import { environment } from '../environments/environment';
+import SimpleBar from "simplebar";
+import {OverlayScrollbars} from "overlayscrollbars";
 
 @Component({
   selector: 'app-root',
@@ -15,13 +17,24 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'player analyzer wizard'
   readonly supabaseService: SupabaseService = new SupabaseService()
 
   session = this.supabaseService.getSession
 
+	constructor(readonly renderer: Renderer2) {
+	}
+
   ngOnInit() {
     this.supabaseService.authChanges((_, session) => (this.session = session))
-  }
+		const foobar = OverlayScrollbars(document.body, {});
+
+	}
+
+	ngAfterViewInit() {
+		// let body = document.body
+		// this.renderer.addClass(body, 'simplebar')
+		// new SimpleBar(body);
+	}
 }
